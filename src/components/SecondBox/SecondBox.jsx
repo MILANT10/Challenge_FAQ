7f02a95823145ee35233a157eefdea58ed698f91
Milan TURCANO <turcano.milan@sfr.fr>
2023-08-01 13:55:03 +0200

    Mis a jour de la box SVG

--- src/components/SecondBox/SecondBox.jsx
+++ src/components/SecondBox/SecondBox.jsx
@@ -4,7 +4,7 @@ import { Accordion } from "../Accordion/Accordion";
 export function SecondBox() {
   return (
     <>
-      <div className="w-11/12 relative bottom-48 flex flex-col items-center p-2 lg:right-[650px] lg:w-full lg:p-0 lg:flex-col">
+      <div className="w-11/12 relative bottom-48 flex flex-col items-center p-2 lg:w-full lg:bottom-0 lg:flex-col">
         <h1 className="text-veryDark text-3xl font-bold mb-5">FAQ</h1>
         <Accordion />
       </div>
--- src/components/SvgCompo/SvgCompo.jsx
+++ src/components/SvgCompo/SvgCompo.jsx
@@ -8,41 +8,41 @@ import cubeSvg from "../../images/illustration-box-desktop.svg";
 export function SvgCompo() {
   if (window.innerWidth >= 1024) {
     return (
-      <>
+      <div className="flex flex-col items-start justify-start h-full overflow-clip">
         <img
-          className="lg:relative lg:w-2/4 lg:right-20"
+          className="lg:relative lg:right-24 lg:top-28 lg:w-full"
           src={womanOnlineDesk}
           alt="A hotline woman on a pc tchating"
         />
 
         <img
-          className="lg:w-2/4 lg:relative lg:right-[600px]"
+          className="lg:relative lg:bottom-64 right-24"
           src={bgPatternDesktop}
           alt="shadow of the hotline woman"
         />
 
         <img
-          className="lg:relative lg:right-[1100px] lg:top-12 lg:z-10"
+          className="lg:absolute lg:top-[460px] left-[180px]"
           src={cubeSvg}
           alt="cube with an @"
         />
-      </>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col items-center relative bottom-40">
+        <img
+          className="w-3/4 p-0 m-0 relative top-14"
+          src={womanOnlineMobileImg}
+          alt="A hotline woman on a pc tchating"
+        />
+
+        <img
+          className="w-3/4 p-0 m-0 relative bottom-6"
+          src={bgPatternMobile}
+          alt="shadow of the hotline women"
+        />
+      </div>
     );
   }
-
-  return (
-    <div className="flex flex-col items-center relative bottom-40">
-      <img
-        className="w-3/4 p-0 m-0 relative top-14"
-        src={womanOnlineMobileImg}
-        alt="A hotline woman on a pc tchating"
-      />
-
-      <img
-        className="w-3/4 p-0 m-0 relative bottom-6"
-        src={bgPatternMobile}
-        alt="shadow of the hotline women"
-      />
-    </div>
-  );
 }
